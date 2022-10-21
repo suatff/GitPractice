634869f09b68b0ba49e61efd157b8e266c547bcd
suatff <102978126+suatff@users.noreply.github.com>
2022-10-21 22:10:55 +0200

    Day1 created

--- src/test/java/suat/Day1.java
+++ src/test/java/suat/Day1.java
@@ -3,5 +3,7 @@ package suat;
 public class Day1 {
     public static void main(String[] args) {
         System.out.println("Crayz work on IT");
+
+        System.out.println("It will good");
     }
 }
